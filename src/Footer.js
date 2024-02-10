6a4644a1e528794764737285dd08d496718e11cc
joemiddie13 <joemiddie13@gmail.com>
2024-02-09 22:29:24 -0800

    stretch challenges for thinking in components

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import './Footer.css';
 
+const currentYear = new Date().getFullYear();
+
 function Footer() {
   return (
-    <div className='Footer'>
-      <h3>Joseph Paul Copyright 2024</h3>
+    <div className="Footer">
+      <div className="copyright">Joseph Paul Copyright {currentYear}</div>
+      <div className="links">
+        <a href="https://example.com/link1">Link 1</a>
+        <a href="https://example.com/link2">Link 2</a>
+        <a href="https://example.com/link3">Link 3</a>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Footer
